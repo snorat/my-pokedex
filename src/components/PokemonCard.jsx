be3b4774b307bcd2c7a52bea5842c6f65b046f11
snorat <saimanorat1@gmail.com>
2023-08-22 12:09:25 +0200

    premier composant

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+function PokemonCard() {
+  return (
+    <div>
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Elephant at sunset" />
+            <figcaption>bulbasaur</figcaption>
+        </figure>
+    </div>
+  );
+}
+
+export default PokemonCard;
