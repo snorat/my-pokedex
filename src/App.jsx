b3093915081777e8d2ae51b073b1d6780c6d90e4
snorat <saimanorat1@gmail.com>
2023-08-23 12:00:59 +0200

    challenge props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  const pokemon = pokemonList[0]
   return (
     <div>
-     <PokemonCard/>
+     < PokemonCard pokemonList={pokemon} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,25 +1,14 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard() {
-const pokemon = pokemonList[0]
-// console.log(pokemon.imgSrc)
+function PokemonCard(props) {
+
   return ( 
 // <div>
 // <h1>{pokemon.name}</h1>
 // <img src={pokemon.imgSrc} />
 // </div>
 <div>
-<figure>{pokemon.imgSrc === undefined ? "???" : <img src={pokemon.imgSrc} alt={pokemon.name} /> }
-<h1> {pokemon.name} </h1>
+<figure>{props.pokemonList.imgSrc === undefined ? "???" : <img src={props.pokemonList.imgSrc} alt={props.pokemonList.name} /> }
+<h1> {props.pokemonList.name} </h1>
 </figure>
 </div>
 
